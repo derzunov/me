3938c785cae039f3785cfd0f7e4ad17449b48ad4
derzunov <derzunov.work@gmail.com>
2022-09-10 19:45:30 +0300

    Main page navigation

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 window.addEventListener('load', () => {
     // Как только страница загрузилась, добавляем на контейнер класс
-    const container = document.querySelector('.card')
+    const elementsToActivate = document.querySelectorAll( '.is-not-active' )
     // Небольшая задержка чтобы все исходные шрифты стили успели примениться корректно
-    setTimeout(() => container.classList.remove('is-not-active'), 1000)
+    setTimeout( () => {
+        elementsToActivate.forEach(( element ) => {
+            element.classList.remove( 'is-not-active' )
+        })
+    }, 1000 )
 })
+
+$( document.body ).on( 'click', 'a.scrollTo', function() {
+    let href = $( this ).attr( 'href' )
+
+    $( 'html, body' ).animate( {
+        scrollTop: $( href ).offset().top
+    }, {
+        duration: 400,
+        easing: 'swing'
+    } )
+} )
+
+
+
+
