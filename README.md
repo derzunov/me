028fd8f1f73f91a4a5db5d4ff73617820335c7f3
derzunov <derzunov.work@gmail.com>
2023-01-02 14:28:06 +0300

    Updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# [derzunov.github.io/me](https://derzunov.github.io/me)
+  - [main](https://derzunov.github.io/me)
+  - [projects](https://derzunov.github.io/me/exp)
---
+++ app.js
@@ -0,0 +1,53 @@
+window.addEventListener( 'load', () => {
+
+    // С этими элементами работаю постоянно пи скролле, их надо прикопать в замыкание
+    const elementsToActivateOnScroll = document.querySelectorAll( '.is-not-active-on-scroll' )
+    const activateOnWindowLoad = () => {
+        // С этими элементами работаю один раз, не надо их прикапывать
+        document.querySelectorAll( '.is-not-active-on-window-load' ).forEach( ( element ) => {
+            element.classList.remove( 'is-not-active-on-window-load' )
+        } )
+    }
+
+    const intersectionOptions = {
+        rootMargin: '0px',
+        threshold: 0.4
+    }
+    const onIntersect = ( entries, observer ) => {
+        entries.forEach( entry => {
+            if ( entry.isIntersecting ) {
+                entry.target.classList.remove( 'is-not-active-on-scroll' )
+            } else {
+                entry.target.classList.add( 'is-not-active-on-scroll' )
+            }
+        } )
+    }
+    const intersectionObserver = new IntersectionObserver( onIntersect, intersectionOptions )
+
+    elementsToActivateOnScroll.forEach( ( element ) => {
+        intersectionObserver.observe(element)
+    } )
+
+    // Небольшая задержка чтобы все исходные шрифты стили успели примениться корректно
+    setTimeout( () => {
+        activateOnWindowLoad()
+    }, 500 )
+
+    console.info('%cНу-ка, нах, не крути тут ничего!', 'font-size: 30px; color: #f0210d');
+} )
+
+try {
+    $( document.body ).on( 'click', 'a.scrollTo', function ( event ) {
+        event.preventDefault()
+        let href = $( this ).attr( 'href' )
+
+        $( 'html, body' ).animate( {
+            scrollTop: $( href ).offset().top
+        }, {
+            duration: 400,
+            easing: 'swing'
+        } )
+    } )
+} catch ( error ) {
+    console.info( 'jQuery is not imported' )
+}
